bf6426a12225b7ac729c325b209ebc1ef566ae9b
Матвей Александров <mmdproduction8@gmail.com>
2025-12-08 23:06:48 +0300

    vector: add func(resize, push_back, pop_back, get_by_index) create structure

--- vector/vector.c
+++ vector/vector.c
@@ -28,7 +28,7 @@ typedef struct _META
 
 
 typedef struct _vector{
-    META* meta;
+    META* meta; //change to **META
     size_t capacity;
     size_t size;
 } vector;
@@ -49,7 +49,39 @@ vector* create_vector(){
 
     return vec;
 }
+void push_back(vector* vec, META value) {
+    if (vec->meta == NULL) {
+        vec->meta = (META*)malloc(sizeof(META));
+        vec->capacity = 1;
+    } 
+    else if (vec->size >= vec->capacity) {
+        vec->capacity *= 2;
+        vec->meta = (META*)realloc(vec->meta, vec->capacity * sizeof(META));
+    }
+    vec->meta[vec->size] = value;
+    vec->size++;
+}
+META get_by_index(vector* vec, u_int index){
+    if (index >= vec->size) {
+        return (META){};
+    }
+    return vec->meta[index];
+}
+void pop_back(vector* vec) {
+    if (vec->size > 0) {
+        vec->size--;
+    }
+}
+void resize(vector* vec){
+    if(vec->size < vec->capacity && vec->size > 0){
+        vec->capacity = vec->size;
+        void* buf = realloc(vec->meta, vec->size);
+        if(!buf){
+            vec->meta = buf;
+        }
+    }
 
+}
 size_t size(vector* vec){
     return vec->size;
 }
@@ -59,25 +91,27 @@ int is_empty(vector* vec){
 }
 
 
-META* get(vector* vec, meta_type type){
-    META* meta = vec->meta;
-    switch (type)
-    {
-    case _NAME_BUILDER:
-        return meta->name_builder;
-    case _NAME_DISTRICT:
-        return meta->name_district;
-    case _TYPE:
-        return meta->type;
-    case _YEAR:
-        return meta->year_of_build;
-    case _LIFT:
-        return meta->is_lift;
-    case _NUM_FLAT:
-        return meta->num_flat;
-    case _NUM_FLOARS:
-        return meta->num_floars;
-    default:
-        return NULL;
-    }
-}
+// * get(vector* vec, meta_type type){
+//     META* meta = vec->meta;
+//     switch (type)
+//     {
+//     case _NAME_BUILDER:
+//         return meta->name_builder;
+//     case _NAME_DISTRICT:
+//         return meta->name_district;
+//     case _TYPE:
+//         return meta->type;
+//     case _YEAR:
+//         return meta->year_of_build;
+//     case _LIFT:
+//         return meta->is_lift;
+//     case _NUM_FLAT:
+//         return meta->num_flat;
+//     case _NUM_FLOARS:
+//         return meta->num_floars;
+//     case _AVERAGE_AREA:
+//         return meta->average_area;
+//     default:
+//         return NULL;
+//     }
+// }
