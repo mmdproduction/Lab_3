d4a5e3656da8a26a35b906f86cacb04d61cb7ece
Матвей Александров <mmdproduction8@gmail.com>
2025-12-15 09:39:09 +0300

    fix problems with CMake

--- flag/flag.c
+++ flag/flag.c
@@ -1,13 +1,13 @@
 #include"flag.h"
 
 void print_help(){
-    puts("\t-h, --help  Show this info\n");
-    puts("\t-g, --generate=N  Generate N lines random Data\n");
-    puts("\t-s, --sort  Sort data\n");
-    puts("\t-p, --print  Print table of DATA\n");
-    puts("\t-o, --output=FILENAME  output DATA in FILENAME\n");
-    puts("\t-i, --in=FILENAME take DATA from FILENAME\n");
-    puts("\t-t, --type=TYPE  Change TYPE of sort\n");
+    puts("\t-h, --help  Show this info");
+    puts("\t-g, --generate=N  Generate N lines random Data");
+    puts("\t-s, --sort  Sort data");
+    puts("\t-p, --print  Print table of DATA");
+    puts("\t-o, --output=FILENAME  output DATA in FILENAME");
+    puts("\t-i, --in=FILENAME take DATA from FILENAME");
+    puts("\t-t, --type=TYPE  Change TYPE of sort");
 }
 
 int parse_args(int argc, char* argv[], Args* args){
--- main.c
+++ main.c
@@ -6,6 +6,41 @@
 #include"flag/flag.h"
 #include"generate/generate.h"
 
+void processing_input(vector** vec, Args** args){
+    if((*args)->gen){
+            printf("%u",(*args)->num);
+            *vec = generate((*args)->num);
+        }
+        else if((*args)->input_csv){
+            *vec = input_out_csv((*args)->input_file);
+        }
+        else{
+            *vec = input();
+        }
+}
+void processing_sort(vector** vec, Args** args){
+    if((*args)->sort){
+            if(strcmp((*args)->flag, "desc") == 0 || strcmp((*args)->flag, "D") == 0){
+                buble_sort_down(*vec, mtcmp);
+            }
+            else{
+                buble_sort_up(*vec, mtcmp);
+            }
+        }
+}
+
+void processing_output(vector** vec, Args** args){
+    if((*args)->output_csv){
+            output_in_csv(*vec, (*args)->out_file);
+        }
+        else if((*args)->print){
+            print(*vec);
+        }
+        else{
+            output(*vec);
+        }
+}
+
 int main(int argc, char* argv[]){
     Args* args = (Args*)malloc(sizeof(Args));
     parse_args(argc, argv, args);
@@ -15,39 +50,8 @@ int main(int argc, char* argv[]){
         print_help();
     }
     else{
-        if(args->gen){
-            printf("%u",args->num);
-            vec = generate(args->num);
-        }
-        else if(args->input_csv){
-            vec = input_out_csv(args->input_file);
-        }
-        else{
-            vec = input();
-        }
-
-        if(args->sort){
-            if(strcmp(args->flag, "desc") == 0 || strcmp(args->flag, "D") == 0){
-                buble_sort_down(vec, mtcmp);
-            }
-            else{
-                buble_sort_up(vec, mtcmp);
-            }
-        }
-
-        if(args->output_csv){
-            output_in_csv(vec, args->out_file);
-        }
-        else if(args->print){
-            print(vec);
-        }
-        else{
-            output(vec);
-        }
+        processing_input(&vec, &args);
+        processing_sort(&vec, &args);
+        processing_output(&vec, &args);
     }
-
-    // vector* vec_2 = generate(20);
-    // print(vec_2);
-    // quick_sort(vec_2, 0, size(vec_2) - 1, mtcmp);
-    // print(vec_2);
 }
